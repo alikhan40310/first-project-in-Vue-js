70a44752408fde20b8fb711116fc80062e8d5ee0
Ali Khan <alikhan40310@gmail.com>
2022-03-03 13:31:02 +0500

    putting signUpform code in single folder

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import Counter from './components/counter.vue'
-import SignUpForm from './components/SignUpForm.vue'
+import Counter from './components/counter.vue';
+import SignUpForm from './components/signUpForm/SignUpForm.vue';
+import './components/signUpForm/SignUpform.css';
+
+
 </script>
 
 <template>
- 
     <Counter></Counter> 
     <SignUpForm></SignUpForm>
 </template>
